dc9cb5e3b38cf92b401925c38ee90d7fcae47b13
Adolfo Rojas <jradolfo11@gmail.com>
2025-07-20 21:16:50 -0600

    Mi primer commit

---
+++ src/app/contacto/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+const contactoPage: React.FC = () => {
+  return (
+    <div>
+      <h1> Esta sera mi pagina contacto</h1>
+    </div>
+  );
+};
+export default contactoPage;
---
+++ src/components/TestimoniosConponents.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TestimonioComponents: React.FC = () => {
+  return (
+    <div>
+      <h1>Esto es la seccion de testimonio</h1>
+    </div>
+  );
+};
+
+export default TestimonioComponents;
